6a8aa1c99ad0a36b1509337ff515e0e61c391c61
Jyota Snyder <jyotasnyder@Jyotas-MacBook-Pro.local>
2017-05-18 22:22:39 -0700

    setup raggedy initial form for actual input used for job submission

--- traveltime/src/App.js
+++ traveltime/src/App.js
@@ -2,32 +2,50 @@ import React, { Component } from 'react';
 import './App.css';
 import Datetime from 'react-datetime';
 import './react-datetime.css';
+var moment = require('moment');
 
 class App extends Component {
   render() {
     return (<div>
       <h2>TravelTime</h2>
-        <table>
-        <tr>
-        <td><p>Starting time at origin: <Datetime className="startTime" /></p></td>
-        <td><p>Arrival time at destination: <Datetime className="endTime" /></p></td>
-        </tr>
-        </table>
-
-      <CalculatedTimeComponent/></div>
+        <p>Starting time at origin: </p>
+      <CalculateTimeFormComponent/></div>
     );
   }
 }
 
 export default App;
 
-
-class CalculatedTimeComponent extends React.Component{
+class CalculateTimeFormComponent extends React.Component{
   constructor(){
     super();
     this.state = {
-      text: '0',
+      originAddress: "16403 25th Ave SE, Bothell, WA 98012",
+      destAddress: "2600 116th Ave NE, Bellevue, WA 98004",
+      travelTime: 0.0,
+      startTime: new Date(),
+      numberOfOriginHours: 2,
+      numberMinDestHours: 6,
+      numberMaxDestHours: 8
     };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event){
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleDateInputChange = (dateValue) => {
+    this.setState({
+      startTime: dateValue.toDate()
+    });
   }
 
   doPollJobStatus = (jobIdentifier) => {
@@ -85,8 +103,66 @@ class CalculatedTimeComponent extends React.Component{
   renderNormal() {
     return (
       <div>
-        <p><b>Calculated time: </b> {this.state.text}</p>
+      <form>
+        <label>
+          Origin address:
+          <input
+            name="originAddress"
+            type="text"
+            size="100"
+            value={this.state.originAddress}
+            onChange={this.handleInputChange} />
+        </label>
+        <br/>
+        <label>
+          Destination address:
+          <input
+            name="destAddress"
+            type="text"
+            size="100"
+            value={this.state.destAddress}
+            onChange={this.handleInputChange} />
+        </label>
+        <br/>
+        <label>
+        Earliest time to leave origin:
+        <Datetime 
+         inputProps={{name: "startTime"}}
+         value={this.state.startTime} 
+         onChange={this.handleDateInputChange} />
+        </label>
+        <br/>
+        <label>
+          Number of hours beyond start that can leave:
+          <input
+            name="numberOfOriginHours"
+            type="number"
+            value={this.state.numberOfOriginHours}
+            onChange={this.handleInputChange} />
+        </label>
+        <br/>
+        <label>
+          Minimum hours at destination:
+          <input
+            name="numberMinDestHours"
+            type="number"
+            value={this.state.numberMinDestHours}
+            onChange={this.handleInputChange} />
+        </label>
+        <br/>
+        <label>
+          Maximum hours at destination:
+          <input
+            name="numberMaxDestHours"
+            type="number"
+            value={this.state.numberMaxDestHours}
+            onChange={this.handleInputChange} />
+        </label>
+        <br/>
         <button onClick={this.doFetchJobStatus}>Calculate</button>
+      </form>
+        <p><b>Calculated time: </b> {this.state.travelTime}</p>
+        
     </div>
     )
   }

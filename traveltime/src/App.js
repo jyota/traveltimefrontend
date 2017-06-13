f0d1663f10c29323563c321d0c43252336c3bcdd
Jyota Snyder <jyotasnyder@Jyotas-MacBook-Pro.local>
2017-06-12 19:52:20 -0700

    fixes around one-way estimate displays.

--- traveltime/src/App.js
+++ traveltime/src/App.js
@@ -4,7 +4,7 @@ import Datetime from 'react-datetime';
 import './react-datetime.css';
 import PlacesAutocomplete from 'react-places-autocomplete';
 import AlertContainer from 'react-alert';
-import { Button, Col, FormControl, FormGroup, Form, Modal } from 'react-bootstrap';
+import { Button, Checkbox, Col, FormControl, FormGroup, Form, Modal } from 'react-bootstrap';
 
 var moment = require('moment');
 
@@ -45,7 +45,8 @@ class CalculateTimeFormComponent extends React.Component{
       numberMinDestHours: 6,
       numberMaxDestHours: 8,
       jobStatus: "waiting_for_job",
-      showResultModal: false
+      showResultModal: false,
+      destOnly: false
     };
 
     this.alertOptions = {
@@ -136,17 +137,28 @@ class CalculateTimeFormComponent extends React.Component{
           this.setState({jobStatus: responseJson.status});
           setTimeout(() => { this.doPollJobStatus(jobIdentifier) }, 5000);
         }else{
-          this.setState({ 
-            travelTime: Math.round(responseJson.result.dest_to_orig_time) + Math.round(responseJson.result.orig_to_dest_time),
-            origToDestTime: moment(responseJson.result.orig_to_dest).add(Math.round(responseJson.result.dest_to_orig_time), 'minutes').format("MM/DD/YYYY hh:mm A"),
-            destToOrigTime: moment(responseJson.result.dest_to_orig).add(Math.round(responseJson.result.orig_to_dest_time), 'minutes').format("MM/DD/YYYY hh:mm A"),
-            origToDestSummary: responseJson.result.orig_to_dest_summary,
-            destToOrigSummary: responseJson.result.dest_to_orig_summary,
-            origToDestTimeToLeave: moment(responseJson.result.orig_to_dest).format("MM/DD/YYYY hh:mm A"),
-            destToOrigTimeToLeave: moment(responseJson.result.dest_to_orig).format("MM/DD/YYYY hh:mm A"),
-            timeZone: responseJson.result.requested.tz_in.timeZoneName,
-            jobStatus: "waiting_for_job"
-           });
+          if(responseJson.result.requested.orig_to_dest_only == false){
+            this.setState({ 
+              travelTime: Math.round(responseJson.result.dest_to_orig_time) + Math.round(responseJson.result.orig_to_dest_time),
+              origToDestTime: moment(responseJson.result.orig_to_dest).add(Math.round(responseJson.result.orig_to_dest_time), 'minutes').format("MM/DD/YYYY hh:mm A"),
+              destToOrigTime: moment(responseJson.result.dest_to_orig).add(Math.round(responseJson.result.dest_to_orig_time), 'minutes').format("MM/DD/YYYY hh:mm A"),
+              origToDestSummary: responseJson.result.orig_to_dest_summary,
+              destToOrigSummary: responseJson.result.dest_to_orig_summary,
+              origToDestTimeToLeave: moment(responseJson.result.orig_to_dest).format("MM/DD/YYYY hh:mm A"),
+              destToOrigTimeToLeave: moment(responseJson.result.dest_to_orig).format("MM/DD/YYYY hh:mm A"),
+              timeZone: responseJson.result.requested.tz_in.timeZoneName,
+              jobStatus: "waiting_for_job"
+             });
+          }else{
+            this.setState({ 
+              travelTime: Math.round(responseJson.result.orig_to_dest_time),
+              origToDestTime: moment(responseJson.result.orig_to_dest).add(Math.round(responseJson.result.orig_to_dest_time), 'minutes').format("MM/DD/YYYY hh:mm A"),
+              origToDestSummary: responseJson.result.orig_to_dest_summary,
+              origToDestTimeToLeave: moment(responseJson.result.orig_to_dest).format("MM/DD/YYYY hh:mm A"),
+              timeZone: responseJson.result.requested.tz_in.timeZoneName,
+              jobStatus: "waiting_for_job"
+            });
+          }
           this.openResultModal();
         }
       })
@@ -157,14 +169,27 @@ class CalculateTimeFormComponent extends React.Component{
       var url = 'http://traveltime-jobservice.integrated.pro/v1/run_task'
       var departEnd = moment(this.state.startTime);
       departEnd.add(this.state.numberOfOriginHours, 'hours');
-      var post_data = {
-        "depart_start": this.doThisMomentFormat(this.state.startTime), 
-        "depart_end": this.doThisMomentFormat(departEnd), 
-        "depart_loc": this.state.originAddress, 
-        "dest_loc": this.state.destAddress, 
-        "min_mins_loc": this.state.numberMinDestHours * 60, 
-        "max_mins_loc": this.state.numberMaxDestHours * 60, 
-        "traffic_model": this.state.trafficModel}
+      if(this.state.destOnly === false){
+        var post_data = {
+          "depart_start": this.doThisMomentFormat(this.state.startTime), 
+          "depart_end": this.doThisMomentFormat(departEnd), 
+          "depart_loc": this.state.originAddress, 
+          "dest_loc": this.state.destAddress, 
+          "min_mins_loc": this.state.numberMinDestHours * 60, 
+          "max_mins_loc": this.state.numberMaxDestHours * 60, 
+          "traffic_model": this.state.trafficModel,
+          "orig_to_dest_only": this.state.destOnly
+        }
+      }else{
+        var post_data = {
+          "depart_start": this.doThisMomentFormat(this.state.startTime), 
+          "depart_end": this.doThisMomentFormat(departEnd), 
+          "depart_loc": this.state.originAddress, 
+          "dest_loc": this.state.destAddress, 
+          "traffic_model": this.state.trafficModel,
+          "orig_to_dest_only": this.state.destOnly
+        }
+      }
 
       fetch(url,
         {
@@ -196,7 +221,11 @@ class CalculateTimeFormComponent extends React.Component{
     }
   }
 
-  renderNormal() {
+  toggleDestCheckbox = () => {
+    this.setState({ destOnly: !this.state.destOnly });
+  }
+
+  renderNormalFormDetails(){
     const originInputProps = {
       value: this.state.originAddress,
       onChange: this.originChange
@@ -214,11 +243,22 @@ class CalculateTimeFormComponent extends React.Component{
 
     const AutocompleteItem = ({ suggestion }) => (<div><i className="fa fa-map-marker"/>{suggestion}</div>);
 
-    return (
+    if(this.state.destOnly === false){
+      return (
       <div>
       <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
       <Form horizontal>
-       <FormGroup controlId="formHorizontalOriginAddr">
+       <FormGroup controlId="formHorizontalDestOnly">
+        <Col className="text-right" xs={4} md={2}>
+          <b>Origin to destination estimate only: </b>
+        </Col>
+        <Col xs={8} md={10}>
+        <Checkbox 
+            value={this.state.destOnly}
+            onChange={this.toggleDestCheckbox}></Checkbox>
+        </Col>
+       </FormGroup>
+       <FormGroup controlId="formHorizontalOriginAddr">        
         <Col className="text-right" xs={4} md={2}>
           <b>Origin address</b>
         </Col>
@@ -325,6 +365,106 @@ class CalculateTimeFormComponent extends React.Component{
         </Modal>        
     </div>
     )
+    }else{
+      return (
+      <div>
+      <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
+      <Form horizontal>
+       <FormGroup controlId="formHorizontalDestOnly">
+        <Col className="text-right" xs={4} md={2}>
+          <b>Origin to destination estimate only: </b>
+        </Col>
+        <Col xs={8} md={10}>
+        <Checkbox 
+            value={this.state.destOnly}
+            onChange={this.toggleDestCheckbox}></Checkbox>
+        </Col>
+       </FormGroup>
+       <FormGroup controlId="formHorizontalOriginAddr">        
+        <Col className="text-right" xs={4} md={2}>
+          <b>Origin address</b>
+        </Col>
+        <Col xs={8} md={10}>
+          <PlacesAutocomplete inputProps={originInputProps} styles={addrStyles} autocompleteItem={AutocompleteItem} />
+        </Col>
+       </FormGroup>
+       <FormGroup controlId="formHorizontalDestAddr">
+        <Col className="text-right" xs={4} md={2}>
+          <b>Destination address</b>
+        </Col>
+        <Col xs={8} md={10}>
+          <PlacesAutocomplete inputProps={destInputProps} styles={addrStylesNext} autocompleteItem={AutocompleteItem} />
+        </Col>
+       </FormGroup>
+       <FormGroup controlId="formHorizontalTTL">
+        <Col className="text-right" xs={4} md={2}>
+          <b>Earliest time to leave origin</b>
+        </Col>
+        <Col xs={8} md={10}>
+        <Datetime 
+         input={false}
+         inputProps={{name: "startTime"}}
+         value={this.state.startTime} 
+         onChange={this.handleDateInputChange} />
+        </Col>
+       </FormGroup>
+       <FormGroup controlId="formHorizontalOriginFlex">
+        <Col className="text-right" xs={4} md={2}>
+          <b>Latest hours to leave after earliest time</b>
+        </Col>
+        <Col xs={8} md={10}>
+          <input
+            name="numberOfOriginHours"
+            type="number"
+            value={this.state.numberOfOriginHours}
+            onChange={this.handleInputChange} />
+        </Col>
+        </FormGroup>
+        <FormGroup controlId="formHorizontalTrafficModel">
+        <Col className="text-right" xs={4} md={2}>
+          <b>Traffic model</b>
+        </Col>
+        <Col xs={8} md={10}>
+          <FormControl style={{width: '150px'}} componentClass="select" placeholder="best_guess" name="trafficModel" value={this.state.trafficModel} onChange={this.handleInputChange}>
+            <option value="best_guess">Best guess</option>
+            <option value="optimistic">Optimistic</option>
+            <option value="pessimistic">Pessimistic</option>
+          </FormControl>
+        </Col>
+        </FormGroup>
+        <FormGroup controlId="formHorizontalSubmission">
+        <Col xsOffset={4} xs={4} md={2} mdOffset={2}>
+        <Button 
+             bsStyle="primary" 
+             disabled={this.state.jobStatus !== "waiting_for_job"}
+             onClick={this.state.jobStatus === "waiting_for_job" ? this.doFetchJobStatus : null}>
+          {this.state.jobStatus === "waiting_for_job" ? "Run calculation" : "Running..."}
+        </Button>
+        </Col>
+        </FormGroup>
+      </Form>
+      <Modal show={this.state.showResultModal} onHide={this.closeResultModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Calculated results</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <b>Timezone: </b> {this.state.timeZone}<br/>
+            <b>Best time to leave from origin: </b> {this.state.origToDestTimeToLeave}<br/>
+            <b>Origin to destination route: </b> {this.state.origToDestSummary}<br/>
+            <b>Estimated arrival at destination: </b> {this.state.origToDestTime}<br/> 
+            <b>Estimated travel time: </b> {this.state.travelTime} minutes<br/>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.closeResultModal}>Close</Button>
+          </Modal.Footer>
+        </Modal>        
+    </div>
+    )
+    }
+  }
+
+  renderNormal() {
+    return(this.renderNormalFormDetails())    
   }
   render() {
       return this.renderNormal()
